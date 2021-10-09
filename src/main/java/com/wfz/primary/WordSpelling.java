e6cb724ea4f04e5cccb6623e4abe36880de560ba
wfz <zhangweiwei@myai.tech>
2021-10-09 21:25:58 +0800

    拼写单词 202110092125

--- src/main/java/com/wfz/primary/WordSpelling.java
+++ src/main/java/com/wfz/primary/WordSpelling.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 
 public class WordSpelling {
     /*
+
+    拼写单词
+
     给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
 
     假如你可以用 chars 中的『字母』（字符）拼写出 words 中的某个『单词』（字符串），那么我们就认为你掌握了这个单词。
@@ -27,21 +30,64 @@ public class WordSpelling {
 
     public static void main(String[] args) {
 
-        String[] words = new String[]{"cat", "bt", "hat", "tree"};
-        String chars = "atach";
-        HashMap<Character, Integer> charsMap = new HashMap<>();
-        for (int i = 0; i <chars.length() ; i++) {
-            char c = chars.charAt(i);
-            charsMap.put(c,charsMap.getOrDefault(c,0)+1);
-        }
+//        String[] words = new String[]{"cat", "bt", "hat", "tree"};
+//
+//        String chars = "atach";
+
+        String[] words = new String[]{"hello","world","leetcode"};
+
+        String chars = "welldonehoneyr";
 
 
+
+//        HashMap<Character, Integer> charsMap = new HashMap<>();
+//        for (int i = 0; i < chars.length(); i++) {
+//            char c = chars.charAt(i);
+//            charsMap.put(c, charsMap.getOrDefault(c, 0) + 1);
+//        }
+//
+//        System.out.println(charsMap.getOrDefault('a', 0));
+
+        System.out.println(wordSpelling(words, chars));
 
 
     }
 
-    private static int wordSpelling() {
+    private static int wordSpelling(String[] words, String chars) {
 
-        return 0;
+        HashMap<Character, Integer> charsMap = new HashMap<>();
+
+        int result = 0;
+
+        for (char c : chars.toCharArray()) {
+            charsMap.put(c, charsMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (String word : words) {
+
+            boolean flag = true;
+
+            HashMap<Character, Integer> wordMap = new HashMap<>();
+            for (char c : word.toCharArray()) {
+                wordMap.put(c, wordMap.getOrDefault(c, 0) + 1);
+            }
+
+
+            for (Character character : wordMap.keySet()) {
+
+                if (wordMap.getOrDefault(character, 0) > charsMap.getOrDefault(character, 0)) {
+                    flag = false;
+                    break;
+                }
+
+            }
+
+            if (flag) {
+
+                result = result + word.length();
+            }
+        }
+
+        return result;
     }
 }
